6f264396f052b5353c81700b1f93c31142f0da91
Nikolai Maksimov <kolkamaximov@gmail.com>
2024-12-10 20:59:52 +0300

    CreateAccountViewController

---
+++ FYM/FYM/Modules/Login/Account/CreateAccountViewController.swift
@@ -0,0 +1,36 @@
+//
+//  CreateAccountViewController.swift
+//  FYM
+//
+//  Created by Nikolai Maksimov on 07.12.2024.
+//
+
+import UIKit
+
+final class CreateAccountViewController: UIViewController {
+    weak var delegate: AccountViewDelegate? {
+        get { accountView.delegate }
+        set { accountView.delegate = newValue }
+    }
+    // MARK: - Private properties
+    
+    private let viewModel: CreateAccountViewModelProtocol
+    private let accountView = CreateAccountView()
+    
+    // MARK: - Init
+    
+    init(viewModel: CreateAccountViewModelProtocol) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Life cycle
+    
+    override func loadView() {
+        view = accountView
+    }
+}
