1f997184644f9609f23d3e7005b21e12c40ef4bb
Nikolai Maksimov <kolkamaximov@gmail.com>
2024-12-16 15:57:59 +0300

    settings viewmodel

---
+++ FYM/FYM/Modules/Settings/ViewModel/SettingsViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  SettingsViewModel.swift
+//  FYM
+//
+//  Created by Nikolai Maksimov on 16.12.2024.
+//
+
+import Foundation
+
+protocol SettingsViewModelProtocol {}
+
+final class SettingsViewModel: SettingsViewModelProtocol {}
+
+// MARK: - SettingsViewDelegate
+extension SettingsViewModel: SettingsViewDelegate {}
